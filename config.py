4cc95487a9ba3c31d234e8354007dfa981d8c9eb
Khanh Tran <nkhanh.trn@gmail.com>
2019-09-08 04:02:04 -0400

    add ability to create new game

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import boto3
 
 # Replace with your credentials here
+AWS_ID = ''
 ACCESS_KEY = ''
 SECRET_KEY = ''
 REGION = 'ca-central-1'
 EC2_INSTANCE = 'i-036996e2f12d5bca3'
+NEW_GAME_SNAPSHOT = 'snap-063c5afd546c9586a'
 VOLUME_DEVICE = 'xvdb'
 
 ec2 = boto3.Session(
--- start-machine.py
+++ start-machine.py
@@ -1,10 +1,39 @@
 from config import *
 
-# Create new Volume from Snapshot
-volume = ec2.create_volume(
-    SnapshotId='snap-0f6ce914c23901e3a',
-    AvailabilityZone=REGION + 'a'
+# choose game
+games = ec2.describe_snapshots(
+    Filters=[
+        {
+            'Name': 'owner-id',
+            'Values': [AWS_ID]
+        }
+    ]
 )
+print("Choose the game:")
+print('0 - Create new game')
+for i in range(len(games['Snapshots'])):
+    if (games['Snapshots'][i]['SnapshotId'] != NEW_GAME_SNAPSHOT):
+        print(str(i + 1) + " - " + games['Snapshots'][i]['Description'])
+choose = int(input())
+
+# create new game
+if choose == 0:
+    print('How much GB does the new game require?')
+    sizeGB = int(input())
+    volume = ec2.create_volume(
+        SnapshotId=NEW_GAME_SNAPSHOT,
+        Size=sizeGB,
+        AvailabilityZone=REGION + 'a'
+    )
+# start an existing game
+else:
+    gameId = games['Snapshots'][int(input()) - 1]['SnapshotId']
+    volume = ec2.create_volume(
+        SnapshotId='snap-0f6ce914c23901e3a',
+        AvailabilityZone=REGION + 'a'
+    )
+
+# wait until volume is created
 volumeId = volume['VolumeId']
 ec2.get_waiter('volume_available').wait(
     VolumeIds=[volumeId],
